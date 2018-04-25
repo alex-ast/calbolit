e1c27afad8c8eaac556e8f5fa6b10031aa724ebf
Alex Astapchuk <alex.astapchuk@gmail.com>
2018-04-24 22:00:57 -0700

    Done: url parsing, calendar download, accepted events filtering.

--- calbolit.js
+++ calbolit.js
@@ -1,10 +1,15 @@
 var http = require('http');
-//const https = require('https');
 var request = require('request');
 var fs = require('fs');
 var ical = require('ical.js');
 var url = require('url');
 
+/*
+ * Usage:
+ *  https://example.com/calbolit?url=https://www.facebook.com/ical/u.php?uid=1821253602&key=AQCFykZOUBTQdpMF
+ *  https://example.com/calbolit?status=accepted&url=https://www.facebook.com/ical/u.php?uid=1821253602&key=AQCFykZOUBTQdpMF
+ *  
+ */
 
 //
 // Config section
@@ -12,7 +17,13 @@ var url = require('url');
 var testIcalUrl = 'https://www.facebook.com/ical/u.php?uid=1821253602&key=AQCFykZOUBTQdpMF';
 var tmpFile = 'tmp.ical';
 var userAgentString = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36';
-var icalParamName='ical';
+
+
+//
+// Constants
+//
+var urlParamName='url';
+var statusParamName = 'status';
 var icalPath = 'calbolit';
 var icalProtocolPrefix = 'webcal://';
 var httpProtocolPrefix = 'http://';
@@ -33,6 +44,13 @@ var LogDbg = function(msg) {
     console.log('dbg> ' + msg);
 };
 
+var LogErr = function(msg) {
+    if (!logFile) {
+        logFile = fs.createWriteStream(LogFileName);
+    }
+    logFile.write('err> ' + msg + '\n');
+    console.log('err> ' + msg);
+};
 
 //
 //
@@ -48,7 +66,11 @@ var download = function(url, cb) {
     
     // TODO: Add timeout
     // TODO: Limit size
-    var options = { url: url, headers: {'User-Agent': userAgentString} };
+    var options = {
+        url: url,
+        headers: {'User-Agent': userAgentString}
+    };
+
     var req = request.get(options);
 
     // check for request error too
@@ -78,29 +100,15 @@ var download = function(url, cb) {
     });
 };
 
-var cb = function(data, err) {
-    if (data !== null) {
-        LogDbg("Have data: "); // + data)
-    }
-    else {
-        LogDbg("Have error: " + err)
-    }
-};
-
-LogDbg('URL=' + url);
-//download(url, cb);
 
 
-
-function ProcessICAL(body) {
+function ProcessICAL(body, onlyAccepted) {
     LogDbg('ProcessICAL, body='+body);
-    
+
     var jcal = ical.parse(body);
     var vcal = new ical.Component(jcal);
     var vevents = vcal.getAllSubcomponents('vevent');
 
-    LogDbg("Events: " + vevents);
-    
     vcal.removeAllSubcomponents("vevent");
 
     for (var i = 0; i < vevents.length; i++) {
@@ -109,68 +117,62 @@ function ProcessICAL(body) {
         var location = ev.getFirstPropertyValue("location");
         var url = ev.getFirstPropertyValue("url");
         var mystatus = ev.getFirstPropertyValue("partstat");
-        
-        LogDbg("Event, name=" + summary);
-        LogDbg("\t\tlocation=" + location);
-        LogDbg("\t\turl=" + url);
-        LogDbg("\t\tGoing? " + mystatus);
-        
-        if (mystatus !== 'ACCEPTED') {
+
+        if (onlyAccepted && mystatus !== 'ACCEPTED') {
             LogDbg("Removed: " + summary);
-        	vevents.splice(i, 1);
-        	--i;
+            vevents.splice(i, 1);
+            --i;
         }
         else {
             vcal.addSubcomponent(ev);
         }
     }
-    
+
     return vcal;
 }
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    ProcessICAL(body);
-  }
-}
-
-//request(options, callback);
-/*
-var icalData = fs.readFileSync('u1821253602.ics', {encoding: 'utf8'});
-
-var ical = ProcessICAL(icalData);
-LogDbg("====================");
-LogDbg("New Events:");
-LogDbg("" + ical.toString());
-*/
-
 
 http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    
     var reqInfoTmp = url.parse(req.url);
-    if (reqInfoTmp.pathname == '/'+icalPath) {
-    	var n = reqInfoTmp.search.indexOf(icalParamName+'=');
-    	if (n == -1) {
-    		// Malformed request.
-	    	var myPart = reqInfoTmp.search.substring(1);
-	    	var icalURL = "";
-    	}
-    	else {
-	    	var myPart = reqInfoTmp.search.substring(1, n);
-	    	var icalURL = reqInfoTmp.search.substring(n + (icalParamName+'=').length);
-    	}
-    	
-    	// TODO: Replace and try HTTPS first and only fallback to http if HTTPS not available.
-    	if (icalURL.toLowerCase().indexOf(icalProtocolPrefix) === 0) {
-    		icalURL = httpProtocolPrefix + icalURL.substring(icalProtocolPrefix.length); 
-    	}
-    	var reqInfo = url.parse('/?' + myPart, true);
-        res.write("myPart=" + myPart + '\n');
-        res.write("icalURL=" + icalURL + '\n');
-
-        download(icalURL);
-    }
     
-    res.end('Hello World zzz\n');
+    if (reqInfoTmp.pathname == '/'+icalPath) {
+
+        var n = reqInfoTmp.search.indexOf(urlParamName+'=');
+        if (n == -1) {
+            // Malformed request.
+            LogErr('Malformed request: ' + req.url);
+            var vcalStr = 'BEGIN:VCALENDAR\nX-ERROR:Malformed request, no webcal parameter found ('+urlParamName+')\nEND:VCALENDAR';
+            res.writeHead(200, {'Content-Type': 'text/calendar;charset=utf-8', 'Content-Disposition': 'attachment;filename=calendar.ics', 'Content-Length': vcalStr.length});
+            res.end(vcalStr);
+            return;
+        }
+
+        var myPart = reqInfoTmp.search.substring(1, n);
+        var icalURL = reqInfoTmp.search.substring(n + (urlParamName+'=').length);
+        LogDbg('icalURL='+icalURL);
+
+
+        // TODO: Replace and try HTTPS first and only fallback to http if HTTPS not available.
+        if (icalURL.toLowerCase().indexOf(icalProtocolPrefix) === 0) {
+            icalURL = httpProtocolPrefix + icalURL.substring(icalProtocolPrefix.length); 
+        }
+  
+        var reqInfo = url.parse('/?' + myPart, true);
+        var onlyAccepted = ('accepted' === reqInfo.query[statusParamName]);
+
+        LogDbg('Processing iCal. Only accepted? ' + onlyAccepted + '; URL=' + icalURL);
+        download(icalURL, function(data, errMsg) {
+            if (errMsg) {
+                //
+            }
+            var vcal = ProcessICAL(data, onlyAccepted);
+            var vcalStr = vcal.toString();
+            res.writeHead(200, {'Content-Type': 'text/calendar;charset=utf-8', 'Content-Disposition': 'attachment;filename=calendar.ics', 'Content-Length': vcalStr.length});
+            res.end(vcalStr);
+        });
+    }
+    else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World zzz\n');
+    }
 }).listen(1337, '127.0.0.1');
